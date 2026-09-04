be15fafa34cc6b642a85d841ed762b52045ca386
Raju204116 <raju204116@gmail.com>
2026-09-04 22:12:32 +0600

    ct2-p2

--- Contest-2/src/p2-count-even-odd-nums.ts
+++ Contest-2/src/p2-count-even-odd-nums.ts
@@ -4,12 +4,10 @@ Given an array of integers, return an object containing the count of even and od
 
 Example 1
 Input: numbers = [1,2,3,4,5]
-
 Output: {"even":2,"odd":3}
 
 Example 2
 Input: numbers = [10,20,30]
-
 Output: {"even":3,"odd":0}
 
 Constraints
@@ -20,3 +18,23 @@ The length of the `numbers` array will be between 0 and 1000.
 
 
 */
+
+
+function countEvenOdd(numbers: number[]): { even: number; odd: number } {
+
+    let countEven = 0;
+    let countOdd = 0;
+    numbers.map((number) => {
+            if (number % 2 === 0) {
+                countEven++;
+            } else {
+                countOdd++;
+            }
+        
+    })
+
+    return { even: countEven, odd: countOdd }
+}
+
+console.log(countEvenOdd([1, 2, 3, 4, 5]));
+console.log(countEvenOdd([10,20,30]));
